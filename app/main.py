5b271aa33baac140901cf250e23fe69bd645d7e0
vetal <semeniv09@ukr.net>
2025-08-06 20:49:07 +0000

    fix file main.py

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ from typing import Callable
 def cache(func: Callable) -> Callable:
     memory = {}
 
-    def wrapper(*args):
-        if args in memory:
+    def wrapper(*args, **kwargs):
+        key = (args, frozenset(kwargs.items()))
+        if key in memory:
             print("Getting from cache")
-            return memory[args]
+            return memory[key]
         print("Calculating new result")
-        result = func(*args)
-        memory[args] = result
+        result = func(*args, **kwargs)
+        memory[key] = result
         return result
 
     return wrapper
